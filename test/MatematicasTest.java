20367d16979b75174cad121eb05303ecb374aa49
Felix <Felix@DESKTOP-TICAJV3>
2019-11-05 17:41:49 +0100

    1

---
+++ test/MatematicasTest.java
@@ -0,0 +1,44 @@
+
+/
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+public class MatematicasTest {
+
+    public MatematicasTest() {
+    }
+
+    @BeforeAll
+    public static void setUpClass() {
+    }
+
+    @AfterAll
+    public static void tearDownClass() {
+    }
+
+    @BeforeEach
+    public void setUp() {
+    }
+
+    @AfterEach
+    public void tearDown() {
+    }
+
+}
+
+public void testSuma(){
+
+assertEquals(5,Matematicas.suma(2,3));
+}
+
+
+public void testResta(){
+
+assertEquals(1,Matematicas.resta(3,2));
+}
