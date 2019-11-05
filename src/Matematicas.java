240257624720e1fd1d9572c84ba9639695568f7d
Felix <Felix@DESKTOP-TICAJV3>
2019-11-05 17:57:56 +0100

    2

---
+++ src/Matematicas.java
@@ -0,0 +1,21 @@
+
+public class Matematicas {
+    
+    
+    
+    
+public static int suma(int a, int b) {
+ return 5;
+ }
+
+
+
+
+public static int resta(int a, int b) {
+ return 5;
+ }
+
+
+  
+}
+
--- test/MatematicasTest.java
+++ test/MatematicasTest.java
@@ -1,44 +1,40 @@
-
-/
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-
-
+import static junit.framework.Assert.assertEquals;
+import org.junit.Test;
 
 public class MatematicasTest {
 
     public MatematicasTest() {
     }
 
-    @BeforeAll
+    
     public static void setUpClass() {
     }
 
-    @AfterAll
+
     public static void tearDownClass() {
     }
 
-    @BeforeEach
+    
     public void setUp() {
     }
 
-    @AfterEach
+    
     public void tearDown() {
     }
+    
+    
+        @Test
+    public void testSuma(){
 
+        assertEquals(5,Matematicas.suma(2,3));
 }
 
-public void testSuma(){
+    @Test
+    public void testResta(){
 
-assertEquals(5,Matematicas.suma(2,3));
-}
+        assertEquals(5,Matematicas.resta(3,2));
+                            }
+
+                                    }
 
 
-public void testResta(){
-
-assertEquals(1,Matematicas.resta(3,2));
-}
